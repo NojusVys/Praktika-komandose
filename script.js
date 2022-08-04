17b70c91791f69e8179a87e982e1fadda50ccfdb
NojusVys <nojusv@gmail.com>
2022-08-04 18:42:45 +0300

    fixed burger menu bug

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
+const stepperWrapper = document.querySelector(".stepper-wrapper");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    stepperWrapper.classList.toggle("active");
 })
 
 document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    stepperWrapper.classList.remove("active");
 }))
